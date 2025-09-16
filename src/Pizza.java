722a6d20157a81e2a57b6d65b9ae8605a0afc7ac
Aran <aranbhatti98@gmail.com>
2025-09-16 16:55:01 -0500

    Pizza

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,29 +1,27 @@
-// Define a class
 public class Pizza {
-    // Instance variables
     String size;
     String topping;
 
-    // Constructor
+    // constructor
     public Pizza(String pizzaSize, String pizzaTopping) {
         size = pizzaSize;
         topping = pizzaTopping;
     }
 
-    // Method
+    // method
     public void displayPizza() {
         System.out.println("You ordered a " + size + " pizza with " + topping + ".");
     }
 
-    // Main method to run the program
+    // main method
     public static void main(String[] args) {
-        // Create first Pizza object
+        // creating object
         Pizza myPizza1 = new Pizza("Large", "Pepperoni");
 
-        // Create second Pizza object
+        // another object
         Pizza myPizza2 = new Pizza("Medium", "Cheese");
 
-        // Call methods using objects
+        // calling method, with object
         myPizza1.displayPizza();
         myPizza2.displayPizza();
     }
